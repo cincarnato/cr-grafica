a7135a27718d0c0506892551a3345a0a0bdd8ac6
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2018-12-05 16:38:35 -0300

    Modificacion de Email

--- module/Revendedor/src/Controller/PedidoCintaController.php
+++ module/Revendedor/src/Controller/PedidoCintaController.php
@@ -134,7 +134,7 @@ class PedidoCintaController extends AbstractActionController
                 $usuario = $formularioCinta->getUsuario()->getUsername();
                 $this->mailManager()->setFrom("ci.sys.virtual@gmail.com");
                 $this->mailManager()->addTo("cristian.cdi@gmail.com");
-             //   $this->mailManager()->addTo("cristiansapir@hotmail.com");
+                $this->mailManager()->addTo("cristiansapir@hotmail.com");
                 $this->mailManager()->setSubject("Grafica CR Print - Su pedido #".$id." de cinta fue cargado con exito");
                 $this->mailManager()->setTemplate("revendedor/mail/nuevo-pedido",["formularioCinta" => $formularioCinta]);
                 //$this->mailManager()->setBody("Se ha registrado un nuevo pedido con ID ". $id ." de revendedor del usuario ". $usuario);
